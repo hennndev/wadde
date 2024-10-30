f1216dccd42521c42ea9098e68375d9dd9b707d7
hennndev <hendraputra230897@gmail.com>
2024-10-30 23:59:50 +0700

    change some features

---
+++ app/components/Homepage/FrameNine.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const FrameNine = () => {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default FrameNine
---
+++ app/components/Homepage/FrameSeven.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const FrameSeven = () => {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default FrameSeven
